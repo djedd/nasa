a212414ae20b9827ceb4c609b9daaecc73e928b3
Eduardo Prado <eduardoprado@unicauca.edu.co>
2024-02-18 23:38:35 -0500

    NASA003: added utils, fetch and Home updated

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,10 +1,29 @@
 // Libraries
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 // Components
 import Header from '@/components/header';
+import fetchApi from '@/utils/fetch';
 
 const Home = () => {
+  const [todaysImage, setTodaysImage] = useState();
+
+  useEffect(() => {
+    const loadTodaysImage = async () => {
+      try {
+        const data = await fetchApi();
+        setTodaysImage(data);
+      } catch (e) {
+        console.error(e);
+        setTodaysImage(undefined);
+      }
+    };
+
+    loadTodaysImage().catch(null);
+  }, []);
+
+  console.log(todaysImage);
+
   return (
     <View style={styles.container}>
       <Header />
